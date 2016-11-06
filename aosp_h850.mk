bb011d1c5a1292bbb747a90321dae00613c90a63
B--B <dark.schneider.ds@gmail.com>
2016-11-06 19:48:00 +0100

    Initial AOSP bringup

--- aosp_h850.mk
+++ aosp_h850.mk
@@ -21,9 +21,23 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from h815 device
 $(call inherit-product, device/lge/h850/device.mk)
 
+# Inherit some common AOSP stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# Overlays (inherit after vendor/cm to ensure we override it)
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
 # Set those variables here to overwrite the inherited values.
+PRODUCT_NAME := aosp_h850
 PRODUCT_DEVICE := h850
-PRODUCT_NAME := full_h850
 PRODUCT_BRAND := lge
 PRODUCT_MODEL := LG-H850
 PRODUCT_MANUFACTURER := LGE
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_DEVICE="g5" \
+    PRODUCT_NAME="h1_tmo_us" \
+    BUILD_FINGERPRINT="lge/h1_global_com/h1:6.0.1/MMB29M/16166143080f8:user/release-keys" \
+    PRIVATE_BUILD_DESC="h1_global_com-user 6.0.1 MMB29M 16166143080f8 release-keys"
+
+TARGET_VENDOR := LGE
